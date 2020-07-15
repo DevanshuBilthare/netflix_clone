e3a2f606a60721769f38760824beff3506bfe42d
hungrykiller <devanshubilthare821@gmail.com>
2020-07-15 14:26:10 +0530

    Updates

---
+++ precache-manifest.cf3bdb8e3bf9a8a3a149bb1cc07f1faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe1e7ce7d8470bf06a1787e6a15eb49",
+    "url": "/netflix_clone/index.html"
+  },
+  {
+    "revision": "b43ff326d5e3cbccc327",
+    "url": "/netflix_clone/static/css/main.d8260ab5.chunk.css"
+  },
+  {
+    "revision": "3031bc68b75fd43aab5d",
+    "url": "/netflix_clone/static/js/2.967cb47b.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix_clone/static/js/2.967cb47b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43ff326d5e3cbccc327",
+    "url": "/netflix_clone/static/js/main.0cd4bf2e.chunk.js"
+  },
+  {
+    "revision": "e251b39daf37863d341c",
+    "url": "/netflix_clone/static/js/runtime-main.40987e46.js"
+  }
+]);
